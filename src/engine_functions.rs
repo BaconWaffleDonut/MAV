e47e036c5a63cff8f55c381b0464c819104b0a3f
BaconWaffleDonut <baconwaffledonut@proton.me>
2026-06-05 17:48:09 -0700

    created layout for engine functions

--- src/engine_functions.rs
+++ src/engine_functions.rs
@@ -1,28 +1,77 @@
-use std::{
-    cell::RefCell, 
-    error::Error, time::Instant};
-use winit::{
-    application::ApplicationHandler,
-    event::WindowEvent,
-    event_loop::{
-        ActiveEventLoop,
-        EventLoop
-    },
-    window::{
-        Window,
-        WindowAttributes,
-        WindowId
-    },
-};
-use log::*;
-use ash::{
-    ext::debug_utils, 
-    khr::{
-        surface, 
-        swapchain},
-    vk, Device, Entry, Instance,
-};
+
 
 pub fn test() {
     println!("Testing importation of functions.")
 }
+
+//====================
+// Instance
+//====================
+
+//====================
+// Physical Device
+//====================
+
+//====================
+// Logical Device
+//====================
+
+//====================
+// Swapchain
+//====================
+
+//====================
+// Pipeline
+//====================
+
+//====================
+// Framebuffers
+//====================
+
+//====================
+// Command Pool
+//====================
+
+//====================
+// Color Objects
+//====================
+
+//====================
+// Depth Objects
+//====================
+
+//====================
+// Texture
+//====================
+
+//====================
+// Model
+//====================
+
+//====================
+// Buffers
+//====================
+
+//====================
+// Descriptors
+//====================
+
+//==================== 
+// Command Buffers
+//====================
+
+//====================
+// Sync Objects
+//====================
+
+//====================
+// Shared Buffers
+//====================
+
+//====================
+// Shared Images
+//====================
+
+//====================
+// Other Shared
+//====================
